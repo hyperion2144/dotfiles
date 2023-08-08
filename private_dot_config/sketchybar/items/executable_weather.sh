b5d83cc9c8a5755f91cb40b44ab71b4ad11a9344
hyperion <2840956925@qq.com>
2023-08-08 21:50:04 +0800

    bar weather

--- private_dot_config/sketchybar/items/executable_weather.sh
+++ private_dot_config/sketchybar/items/executable_weather.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-POPUP_CLICK_SCRIPT="sketchybar --set weather popup.drawing=toggle"
-
 weather_icon=(
 	drawing=off
 	icon.align=right
@@ -9,23 +7,10 @@ weather_icon=(
 	icon.padding_left=5
 	icon.padding_right=5
 	script="$PLUGIN_DIR/weather.sh"
-	click_script="$POPUP_CLICK_SCRIPT"
+	click_script="open -a Weather"
 	update_freq=60
 	popup.align=right
 )
 
-weather_details=(
-	drawing=off
-	background.corner_radius=12
-	padding_left=7
-	padding_right=7
-)
-
 sketchybar --add item weather.icon right \
-	--set weather.icon "${weather_icon[@]}" \
-	\
-	--add item weather.details popup.weather.icon \
-	--set weather.details "${weather_details[@]}" \
-	--subscribe weather.icon mouse.entered \
-	mouse.exited \
-	mouse.exited.global
+	--set weather.icon "${weather_icon[@]}"
--- private_dot_config/sketchybar/plugins/executable_weather.sh
+++ private_dot_config/sketchybar/plugins/executable_weather.sh
@@ -1,68 +1,126 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-source "$HOME/.config/sketchybar/colors.sh"
+API_KEY="a6343854760645c1ad1124138230808" # insert api key here
+ip=$(curl -s "https://cip.cc" | grep IP | cut -f 2 -d ":" | cut -f 2 -d " ")
+CITY=$(curl -s "https://ifconfig.co/city?ip=${ip}")
 
-render_bar() {
-	sketchybar --set weather.icon icon="${icon}" \
-		--set weather.icon drawing=on
-}
+# first comment is description, second is icon number
+weather_icons_day=(
+	[1000]= # Sunny/113
+	[1003]= # Partly cloudy/116
+	[1006]= # Cloudy/119
+	[1009]= # Overcast/122
+	[1030]= # Mist/143
+	[1063]= # Patchy rain possible/176
+	[1066]= # Patchy snow possible/179
+	[1069]= # Patchy sleet possible/182
+	[1072]= # Patchy freezing drizzle possible/185
+	[1087]= # Thundery outbreaks possible/200
+	[1114]= # Blowing snow/227
+	[1117]= # Blizzard/230
+	[1135]= # Fog/248
+	[1147]= # Freezing fog/260
+	[1150]= # Patchy light drizzle/263
+	[1153]= # Light drizzle/266
+	[1168]= # Freezing drizzle/281
+	[1171]= # Heavy freezing drizzle/284
+	[1180]= # Patchy light rain/293
+	[1183]= # Light rain/296
+	[1186]= # Moderate rain at times/299
+	[1189]= # Moderate rain/302
+	[1192]= # Heavy rain at times/305
+	[1195]= # Heavy rain/308
+	[1198]= # Light freezing rain/311
+	[1201]= # Moderate or heavy freezing rain/314
+	[1204]= # Light sleet/317
+	[1207]= # Moderate or heavy sleet/320
+	[1210]= # Patchy light snow/323
+	[1213]= # Light snow/326
+	[1216]= # Patchy moderate snow/329
+	[1219]= # Moderate snow/332
+	[1222]= # Patchy heavy snow/335
+	[1225]= # Heavy snow/338
+	[1237]= # Ice pellets/350
+	[1240]= # Light rain shower/353
+	[1243]= # Moderate or heavy rain shower/356
+	[1246]= # Torrential rain shower/359
+	[1249]= # Light sleet showers/362
+	[1252]= # Moderate or heavy sleet showers/365
+	[1255]= # Light snow showers/368
+	[1258]= # Moderate or heavy snow showers/371
+	[1261]= # Light showers of ice pellets/374
+	[1264]= # Moderate or heavy showers of ice pellets/377
+	[1273]= # Patchy light rain with thunder/386
+	[1276]= # Moderate or heavy rain with thunder/389
+	[1279]= # Patchy light snow with thunder/392
+	[1282]= # Moderate or heavy snow with thunder/395
+)
 
-render_popup() {
-	sketchybar --remove '/weather.details.\.*/'
+weather_icons_night=(
+	[1000]= # Clear/113
+	[1003]= # Partly cloudy/116
+	[1006]= # Cloudy/119
+	[1009]= # Overcast/122
+	[1030]= # Mist/143
+	[1063]= # Patchy rain possible/176
+	[1066]= # Patchy snow possible/179
+	[1069]= # Patchy sleet possible/182
+	[1072]= # Patchy freezing drizzle possible/185
+	[1087]= # Thundery outbreaks possible/200
+	[1114]= # Blowing snow/227
+	[1117]= # Blizzard/230
+	[1135]= # Fog/248
+	[1147]= # Freezing fog/260
+	[1150]= # Patchy light drizzle/263
+	[1153]= # Light drizzle/266
+	[1168]= # Freezing drizzle/281
+	[1171]= # Heavy freezing drizzle/284
+	[1180]= # Patchy light rain/293
+	[1183]= # Light rain/296
+	[1186]= # Moderate rain at times/299
+	[1189]= # Moderate rain/302
+	[1192]= # Heavy rain at times/305
+	[1195]= # Heavy rain/308
+	[1198]= # Light freezing rain/311
+	[1201]= # Moderate or heavy freezing rain/314
+	[1204]= # Light sleet/317
+	[1207]= # Moderate or heavy sleet/320
+	[1210]= # Patchy light snow/323
+	[1213]= # Light snow/326
+	[1216]= # Patchy moderate snow/329
+	[1219]= # Moderate snow/332
+	[1222]= # Patchy heavy snow/335
+	[1225]= # Heavy snow/338
+	[1237]= # Ice pellets/350
+	[1240]= # Light rain shower/353
+	[1243]= # Moderate or heavy rain shower/356
+	[1246]= # Torrential rain shower/359
+	[1249]= # Light sleet showers/362
+	[1252]= # Moderate or heavy sleet showers/365
+	[1255]= # Light snow showers/368
+	[1258]= # Moderate or heavy snow showers/371
+	[1261]= # Light showers of ice pellets/374
+	[1264]= # Moderate or heavy showers of ice pellets/377
+	[1273]= # Patchy light rain with thunder/386
+	[1276]= # Moderate or heavy rain with thunder/389
+	[1279]= # Patchy light snow with thunder/392
+	[1282]= # Moderate or heavy snow with thunder/395
+)
 
-	COUNTERR=0
-	for line in "${popup_weather[@]}"; do
-		weather_line=(
-			label="$line"
-			label.font="$FONT:Bold:12.0"
-			label.drawing=on
-			click_script="sketchybar --set $NAME popup.drawing=off"
-			drawing=on
-		)
-		item=weather.details."$COUNTERR"
+data=$(curl -s "http://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$CITY")
+condition=$(echo "$data" | jq -r '.current.condition.code')
+temp=$(echo "$data" | jq -r '.current.temp_c')
+feelslike=$(echo "$data" | jq -r '.current.feelslike_c')
+humidity=$(echo "$data" | jq -r '.current.humidity')
+is_day=$(echo "$data" | jq -r '.current.is_day')
 
-		sketchybar --add item "$item" popup.weather.icon
-		sketchybar --set "$item" "${weather_line[@]}"
+if [ "$is_day" = "1" ]; then
+	icon=${weather_icons_day[$condition]}
+else
+	icon=${weather_icons_night[$condition]}
+fi
 
-		COUNTERR=$((COUNTERR + 1))
-	done
-}
-
-update() {
-	# Bar
-	ip=$(curl -s -4 cip.cc | grep IP | cut -f 2 -d ":" | cut -f 2 -d " ")
-	url="https://wttr.in/$ip?format=%c%t&lang=zh"
-	icon=$(curl -s "$url" | sed 's/  */ /g')
-
-	# popup
-	url="https://wttr.in/$ip?T&lang=zh"
-	IFS=$'\n'
-	popup_weather=($(curl -s "$url"))
-	IFS=$OIFS
-
-	render_bar
-	render_popup
-
-	if [ "$COUNT" -ne "$PREV_COUNT" ] 2>/dev/null || [ "$SENDER" = "forced" ]; then
-		sketchybar --animate tanh 15 --set "$NAME" label.y_offset=5 label.y_offset=0
-	fi
-}
-
-popup() {
-	sketchybar --set "$NAME" popup.drawing="$1"
-}
-
-case "$SENDER" in
-"routine" | "forced")
-	update
-	;;
-"mouse.entered")
-	popup on
-	;;
-"mouse.exited" | "mouse.exited.global")
-	popup off
-	;;
-"mouse.clicked")
-	popup toggle
-	;;
-esac
+sketchybar --set "$NAME" \
+	icon="$icon" \
+	label="${temp}°C" \
+	drawing=on
