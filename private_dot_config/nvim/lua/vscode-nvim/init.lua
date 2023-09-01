01e56b767406373998ff5993392b9a1d1c05ec6c
hyperion2144 <2840956925@qq.com>
2023-09-01 14:39:19 +0800

    vscode

--- private_dot_config/nvim/lua/vscode-nvim/init.lua
+++ private_dot_config/nvim/lua/vscode-nvim/init.lua
@@ -1,3 +1,10 @@
 require('vscode-nvim.keybindings')
 require('vscode-nvim.options')
 require('vscode-nvim.lazy')
+
+vim.api.nvim_exec([[
+augroup autosave
+    autocmd!
+    autocmd InsertLeave * call VSCodeNotify("workbench.action.files.save")
+augroup end
+]], false)
