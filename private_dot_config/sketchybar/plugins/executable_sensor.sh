c0cbb65427ff3441cb05caab174e87d8558451bd
hyperion2144 <2840926925@qq.com>
2023-09-03 10:40:23 +0800

    bar sensor

--- private_dot_config/sketchybar/plugins/executable_sensor.sh
+++ private_dot_config/sketchybar/plugins/executable_sensor.sh
@@ -8,11 +8,10 @@ GPU_TEMP=$(
 	istats scan TG0D | cut -f 2 -d "=" | cut -f 1 -d "." | sed 's/[[:space:]]//g'
 )
 
-if [ "$CPU_TEMP" -eq "0" ]; then
+if [ "$CPU_TEMP" == "0" ] || [ "$GPU_TEMP" == "" ]; then
 	sketchybar -m --set sensor.cpu drawing=off \
 		--set sensor.gpu drawing=off
 else
 	sketchybar -m --set sensor.cpu drawing=on label="${CPU_TEMP}" icon.highlight=$(if [ "$CPU_TEMP" -gt "70" ]; then echo "on"; else echo "off"; fi) \
 		--set sensor.gpu drawing=on label="$GPU_TEMP" icon.highlight=$(if [ "$GPU_TEMP" -gt "70" ]; then echo "on"; else echo "off"; fi)
-
 fi
